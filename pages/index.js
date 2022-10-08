e3aa4ea802d743270befb5fa382ec74ce084c188
Ernesto Mejias <ernepazzo1212@gmail.com>
2022-10-07 21:45:50 -0400

    primer

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+import { Button } from "@nextui-org/react";
 
 export default function HomePage() {
   return (
     <>
-      <h1>Hola Mundo</h1>
+      <Button color="gradient">
+        Hola Mundo
+      </Button>
     </>
   )
 }
